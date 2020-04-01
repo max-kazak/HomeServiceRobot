ca6f697d7229317514fcfe72b9a86e9e17be7b23
max-kazak <m@xkazak.com>
2020-04-01 01:13:58 -0600

    +home_service.sh

---
+++ scripts/home_service.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+export ROBOT_INITIAL_POSE="-x 0 -y 0 -Y 3.14"
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 10
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch" &
+sleep 5
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+xterm -e " rosrun add_markers add_markers" &
+sleep 5
+xterm -e " rosrun pick_objects pick_objects"
