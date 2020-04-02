8f96aacbc48878d65c5f3dc3d5768598ebff3c20
max-kazak <m@xkazak.com>
2020-04-01 19:37:08 -0600

    fix scripts: increase time for gazebo to  launch to 10s

--- scripts/add_markers.sh
+++ scripts/add_markers.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export ROBOT_INITIAL_POSE="-x 0 -y 0 -Y 3.14"
 xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch" &
-sleep 5
+sleep 10
 xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch" &
 sleep 5
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+export ROBOT_INITIAL_POSE="-x 0 -y 0 -Y 3.14"
 xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch " &
-sleep 5
+sleep 10
 xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch " &
+# xterm -e " roslaunch gmapping slam_gmapping_pr2.launch " &
 sleep 5
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 5
